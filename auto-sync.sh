239a68a93206589c77e2ed74856e9a74a21fb96f
mfrancis95 <mikefrancis95@gmail.com>
2018-01-16 16:35:59 -0500

    Ensure that the first argument is a directory

--- auto-sync.sh
+++ auto-sync.sh
@@ -1,14 +1,16 @@
 show_usage() {
     echo 'Usage: auto-sync directory destination...'
+    exit $1
 }
 
 if [ $# -eq 0 ]; then
-    show_usage
-    exit 1
+    show_usage 1
 elif [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
-    show_usage
+    show_usage 0
 elif [ $# -eq 1 ]; then
-    show_usage
+    show_usage 1
+elif [ ! -d $1 ]; then
+    echo "$1 is not a directory."
     exit 1
 else
     while true; do
